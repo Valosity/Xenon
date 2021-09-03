53ec3a9633d7c33091d37d4179ee4455bf05ef52
Ian Gainey <iantgainey@gmail.com>
2021-09-02 21:33:31 -0400

    Rendering player with position and sprite centering

--- Xenon/main.cpp
+++ Xenon/main.cpp
@@ -6,7 +6,7 @@ void draw()
 }
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(650, 700), "Xenon", sf::Style::Default);
+    sf::RenderWindow window(sf::VideoMode(650, 700), "Xenon", sf::Style::Titlebar | sf::Style::Close);
 
     while (window.isOpen()) {
         sf::Event ev;
@@ -27,9 +27,15 @@ int main()
             //Error
             printf("No image for Player found");
         }
-        plyrImg.setSmooth(true);
         sf::Sprite plyrSpr;
         plyrSpr.setTexture(plyrImg);
+        unsigned int plyrWidth = plyrSpr.getTexture()->getSize().x;
+        unsigned int plyrHeight = plyrSpr.getTexture()->getSize().y;
+        plyrSpr.setOrigin(plyrWidth / 2, plyrHeight / 2);
+        sf::Vector2u size = window.getSize();
+        unsigned int width = size.x;
+        unsigned int height = size.y;
+        plyrSpr.setPosition(width / 2, height - 100);
         window.draw(plyrSpr);
         window.display();
         
